feaf4ce2241de7048b19a08425f302ae94b6f59f
Uzma <uzmaahsan27@gmail.com>
2021-05-27 19:59:08 -0400

    Add address class

---
+++ src/day47_constructor/Address.java
@@ -0,0 +1,53 @@
+package day47_constructor;
+
+public class Address {
+    private String street;
+    private String city;
+   private  String state;
+    private String zipcode;
+   private  String country = "USA";
+
+   public String toString(){
+       return street + " ," + " ," + state + zipcode + country;
+   }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
