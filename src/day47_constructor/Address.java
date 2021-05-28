5bfe36ee3ce8a25e5042971bf18dd56d5eb11b8a
Uzma <uzmaahsan27@gmail.com>
2021-05-27 22:11:56 -0400

    Studen

--- src/day47_constructor/Address.java
+++ src/day47_constructor/Address.java
@@ -1,22 +1,42 @@
 package day47_constructor;
-
 public class Address {
     private String street;
     private String city;
-   private  String state;
-    private String zipcode;
-   private  String country = "USA";
+    private String state;
+    private String zipCode;
+    private String country = "USA";
+    //constructor method- automatically called
+    public Address() {
+        System.out.println("Address constructor");
+        street = "123 unknown st";
+        city = "Unknown";
+        state = "Unknown";
+        zipCode = "00000";
+    }
+    //second constructor, overloaded constructor -> provides shortcut to initialize variables
+    //in same statement: ex: Address ad = new Address("123 java st", "Boston", "MA", "43213");
+    public Address(String street, String city, String state, String zipCode) {
+        setStreet(street);//reuse the code in the setter method
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
 
-   public String toString(){
-       return street + " ," + " ," + state + zipcode + country;
-   }
+    public String toString() {
+        return street + ", " + city + ", " + state + " " + zipCode;
+    }
 
     public String getStreet() {
         return street;
     }
 
     public void setStreet(String street) {
-        this.street = street;
+        if(street.isEmpty() || street.length() > 50) {
+            System.out.println("ERROR: Invalid street");
+            //System.exit(0);
+        } else {
+            this.street = street;
+        }
     }
 
     public String getCity() {
@@ -35,12 +55,12 @@ public class Address {
         this.state = state;
     }
 
-    public String getZipcode() {
-        return zipcode;
+    public String getZipCode() {
+        return zipCode;
     }
 
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
     }
 
     public String getCountry() {
