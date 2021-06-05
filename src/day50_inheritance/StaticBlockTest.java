2a3d17d63778f8ef302b28b55b8370840f31361a
Uzma <uzmaahsan27@gmail.com>
2021-06-05 12:31:19 -0400

    Inheritance

---
+++ src/day50_inheritance/StaticBlockTest.java
@@ -0,0 +1,10 @@
+package day50_inheritance;
+
+public class StaticBlockTest {
+    public static void main(String[] args) {
+        StaticBlockDemo st1 = new StaticBlockDemo();
+        StaticBlockDemo st2 = new StaticBlockDemo();
+        StaticBlockDemo st3 = new StaticBlockDemo();
+        System.out.println(StaticBlockDemo.num);
+    }
+}
